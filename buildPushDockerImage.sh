480036fb06c798db542513cf0f5b2891806b82ed
hushenglang <hushenglang@gmail.com>
2018-06-01 15:28:36 +0800

    add docker support for travisCI

--- buildPushDockerImage.sh
+++ buildPushDockerImage.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 docker build -f Dockerfile -t service1 .
-docker login -u hushenglang -p hushenglang123
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 docker tag service1 hushenglang/service1
 docker push hushenglang/service1

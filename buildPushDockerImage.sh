dedd4e3ede87d27ca075ea8bddcf8f4ad8a9b818
hushenglang <hushenglang@gmail.com>
2018-06-01 16:00:42 +0800

    update config push to private docker registry

--- buildPushDockerImage.sh
+++ buildPushDockerImage.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 docker build -f Dockerfile -t service1 .
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+#docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 docker tag service1 hushenglang/service1
-docker push hushenglang/service1
+docker push http://18.144.4.65:5000/hushenglang/service1
